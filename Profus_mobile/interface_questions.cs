833692b0611faf332811acaf2ad51ad48378f44c
Zachary Hollands <72099106+Zach-txt@users.noreply.github.com>
2020-11-20 18:20:58 -0500

    oihji

---
+++ Profus_mobile/interface_questions.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Profus_mobile
+{
+    [Activity(Label = "interface_questions")]
+    public class interface_questions : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.interface_questions);
+            // Create your application here
+
+        }
+    }
+}
